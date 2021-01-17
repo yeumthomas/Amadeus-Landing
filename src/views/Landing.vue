a495ca7759b900ff093c29d96fffc07fdd72e67b
Thomas Yeum <yeum.thomas@gmail.com>
2021-01-17 03:45:26 -0600

    edit profile

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -149,7 +149,7 @@
             <div class="shape shape-style-1 shape-default shape-skew">
             </div>
             <div class="container">
-                <div class="row pt-7 mb-5 align-self-start">
+                <div class="row pt-7 mb-3 align-self-start">
                     <div class="col text-center align-self-start">
                         <icon name="ni ni-settings" class=" align-items-center align-self-start bg-gradient-white" size="lg" type="primary" shadow
                                   rounded></icon>
@@ -157,12 +157,7 @@
                     </div>
                 </div>
                 <div class="row row-grid align-items-center">
-                    <div class="col-md-6 order-lg-1 ml-lg-auto">
-                        <div class="position-relative pl-md-5">
-                            <img src="img/ill/ill-2.svg" class="img-center img-fluid">
-                        </div>
-                    </div>
-                    <div class="col-lg-6 order-lg-2">
+                    <div class="col-lg-12 order-lg-2">
                         <card shadow class="shadow-lg--hover mt-5">
                             <div class="d-flex px-3">
                                 <div>
@@ -180,12 +175,7 @@
                     </div>
                 </div>
                 <div class="row row-grid align-items-center">
-                    <div class="col-md-6 order-lg-2 ml-lg-auto">
-                        <div class="position-relative pl-md-5">
-                            <img src="img/ill/ill-2.svg" class="img-center img-fluid">
-                        </div>
-                    </div>
-                    <div class="col-lg-6 order-lg-1">
+                    <div class="col-lg-12 order-lg-1">
                         <card shadow class="shadow-lg--hover mt-5">
                             <div class="d-flex px-3">
                                 <div>
@@ -202,12 +192,7 @@
                     </div>
                 </div>
                 <div class="row row-grid align-items-center">
-                    <div class="col-md-6 order-lg-1 ml-lg-auto">
-                        <div class="position-relative pl-md-5">
-                            <img src="img/ill/ill-2.svg" class="img-center img-fluid">
-                        </div>
-                    </div>
-                    <div class="col-lg-6 order-lg-2">
+                    <div class="col-lg-12 order-lg-2">
                         <card shadow class="shadow-lg--hover mt-5">
                             <div class="d-flex px-3">
                                 <div>
@@ -232,57 +217,48 @@
             <div class="container">
                 <div class="row justify-content-center text-center mb-lg">
                     <div class="col-lg-8">
-                        <h2 class="display-3">Amadeus was developed by Thomas Yeum</h2>
-                        <p class="lead text-muted">According to the National Oceanic and Atmospheric Administration,
-                            Ted, Scambos, NSIDClead scentist, puts the potentially record maximum.</p>
+                        <h2 class="display-3">Amadeus was developed by</h2>
+                        <h4 class="lead">A Student. Programmer. Violist. Composer.</h4>
+                        <p class="lead text-muted">Hey, I'm Thomas. I made this service after I saw the impact of COVID-19 on
+                                                    music education in America. This was the solution I came up with.
+                        </p>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-md-6 col-lg-3 mb-5 mb-lg-0">
-                        <div class="px-4">
-                            <img v-lazy="'img/theme/team-1-800x800.jpg'"
-                                 class="rounded-circle img-center img-fluid shadow shadow-lg--hover"
-                                 style="width: 200px;">
-                            <div class="pt-4 text-center">
-                                <h5 class="title">
-                                    <span class="d-block mb-1">Ryan Tompson</span>
-                                    <small class="h6 text-muted">Web Developer</small>
-                                </h5>
-                                <div class="mt-3">
-                                    <base-button tag="a" href="#" type="warning" icon="fa fa-twitter" rounded
-                                                 icon-only></base-button>
-                                    <base-button tag="a" href="#" type="warning" icon="fa fa-facebook" rounded
-                                                 icon-only></base-button>
-                                    <base-button tag="a" href="#" type="warning" icon="fa fa-dribbble" rounded
-                                                 icon-only></base-button>
-                                </div>
-                                <div class="row row-grid align-items-center my-md">
-                                    <div class="col-lg-6 text-lg-center btn-wrapper">
-                                        <a target="_blank" rel="noopener" href="https://twitter.com/creativetim"
-                                        class="btn btn-neutral btn-icon-only btn-twitter btn-round btn-lg" data-toggle="tooltip"
-                                        data-original-title="Follow us">
-                                            <i class="fa fa-twitter"></i>
-                                        </a>
-                                        <a target="_blank" rel="noopener" href="https://www.facebook.com/creativetim"
-                                        class="btn btn-neutral btn-icon-only btn-facebook btn-round btn-lg" data-toggle="tooltip"
-                                        data-original-title="Like us">
-                                            <i class="fa fa-facebook-square"></i>
-                                        </a>
-                                        <a target="_blank" rel="noopener" href="https://dribbble.com/creativetim"
-                                        class="btn btn-neutral btn-icon-only btn-dribbble btn-lg btn-round" data-toggle="tooltip"
-                                        data-original-title="Follow us">
-                                            <i class="fa fa-dribbble"></i>
-                                        </a>
-                                        <a target="_blank" rel="noopener" href="https://github.com/creativetimofficial"
-                                        class="btn btn-neutral btn-icon-only btn-github btn-round btn-lg" data-toggle="tooltip"
-                                        data-original-title="Star on Github">
-                                            <i class="fa fa-github"></i>
-                                        </a>
-                                    </div>
-                                </div>
+                 <div class="col-md-6 mr-md-auto ml-md-auto mt--100">
+                    <div class="justify-content-center">
+                        <img src="img/thomas_yeum.jpg" class="shadow shadow-lg--hover rounded-circle img-center img-fluid"
+                                style="width: 300px;">
+                    </div>
+                    <div class="pt-4 text-center">
+                        <h5 class="title">
+                            <span class="d-block mb-1">Thomas Yeum</span>
+                            <small class="h6 text-muted">Founder, Developer</small>
+                        </h5>
+                        <div class="col mt-3">
+                            <div class="justify-content-center align-items-center text-center btn-wrapper">
+                                <a target="_blank" rel="noopener" href="https://www.linkedin.com/in/yeumthomas/"
+                                class="rounded-circle btn btn-neutral btn-icon-only btn-twitter btn-round btn-lg" data-toggle="tooltip"
+                                data-original-title="">
+                                    <i class="fa fa-linkedin"></i>
+                                </a>
+                                <a target="_blank" rel="noopener" href="https://www.facebook.com/yeumthomas"
+                                class="rounded-circle btn btn-neutral btn-icon-only btn-facebook btn-round btn-lg" data-toggle="tooltip"
+                                data-original-title="">
+                                    <i class="fa fa-facebook-square"></i>
+                                </a>
+                                <a target="_blank" rel="noopener" href="https://www.instagram.com/thomasyeum/?hl=en"
+                                class="rounded-circle btn btn-neutral btn-icon-only btn-instagram btn-lg btn-round" data-toggle="tooltip"
+                                data-original-title="">
+                                    <i class="fa fa-instagram"></i>
+                                </a>
+                                <a target="_blank" rel="noopener" href="https://www.youtube.com/channel/UCk3xwqtCowaHghNkaZoIogw"
+                                class="rounded-circle btn btn-neutral btn-icon-only btn-youtube btn-round btn-lg" data-toggle="tooltip"
+                                data-original-title="">
+                                    <i class="fa fa-youtube"></i>
+                                </a>
                             </div>
                         </div>
-                    </div>            
+                    </div>
                 </div>
             </div>
         </section>
@@ -298,14 +274,15 @@
                     <div class="p-5">
                         <div class="row align-items-center">
                             <div class="col-lg-8">
-                                <h3 class="text-white">We made making virtual ensembles easier for you.</h3>
-                                <p class="lead text-white mt-3">I will be the leader of a company that ends up being
-                                    worth billions of dollars, because I got the answers. I understand culture.</p>
+                                <h3 class="text-white">We made making virtual ensembles easier.</h3>
+                                <p class="lead text-white mt-3">With the orchestras, choirs, bands in mind, we provide 
+                                    a by musicians for musicians experience to simplify this process. We hope you 
+                                    enjoy it!</p>
                             </div>
                             <div class="col-lg-3 ml-lg-auto">
-                                <base-button tag="a" href="https://www.creative-tim.com/product/vue-argon-design-system"
+                                <base-button tag="a" href="/pricing"
                                              type="white" block size="lg">
-                                    Register Now
+                                    View Pricing
                                 </base-button>
                             </div>
                         </div>
